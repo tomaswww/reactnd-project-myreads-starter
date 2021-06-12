4da4c4720699b1a0f6d4e175b8e2307b6940cf28
tomaswww <twingord@gmail.com>
2021-06-12 01:28:58 -0300

    Fixed ShelfChanger values

--- src/Book.js
+++ src/Book.js
@@ -2,11 +2,12 @@ import React from 'react'
 import ShelfChanger from './ShelfChanger'
 
 const Book = (props) => {
+  console.log(props)
     return (
             <div className="book">
               <div className="book-top">
                 <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url('+props.image+')'}}></div>
-                <ShelfChanger />
+                  <ShelfChanger shelf={props.shelf} />
               </div>
               <div className="book-title">{props.title}</div>
               <div className="book-authors">{props.author}</div>
--- src/ShelfChanger.js
+++ src/ShelfChanger.js
@@ -1,20 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const ShelfChanger = (props) => {
+class ShelfChanger extends Component {
+  constructor(props) {
+    super(props);
+    console.log(props)
+    this.state = {value: props.shelf};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('New shelf is: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
     return (
             <div className="book">
                 <div className="book-shelf-changer">
-                  <select>
-                    <option value="move" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                  </select>
+                      <select value={this.state.value} onChange={this.handleChange}>
+                        <option value="move" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                      </select>
                 </div>
               </div>
     )
-}
+  }
+};
                    
 
 export default ShelfChanger
