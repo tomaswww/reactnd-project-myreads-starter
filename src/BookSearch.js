70e9765a74029b0856d4487716677481b77743e2
tomaswww <twingord@gmail.com>
2021-06-13 01:09:33 -0300

    Added search functionality and results

---
+++ src/BookSearch.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react'
+import ShelfChanger from './ShelfChanger'
+import * as BooksAPI from './BooksAPI'
+import { Link } from 'react-router-dom'
+import './App.css'
+import Book from './Book'
+
+class BookSearch extends Component {
+  state = {
+    books: []
+  }
+
+  getResults(query) {
+    BooksAPI.search(query)
+      .then((books) => {
+        console.log(books)
+        if (typeof books !== 'undefined'){
+          if (books.length > 0){
+            this.setState(() => ({
+              books
+          }))
+          }
+          else {
+          books = []
+          this.setState(() => ({
+              books
+          }))
+        }
+        }
+        else {
+          books = []
+          this.setState(() => ({
+              books
+          }))
+        }
+      })
+      
+  }
+  render(){
+    return (
+            <div className="search-books">
+            <div className="search-books-bar">
+              <Link to='/'>
+                <button className="close-search">Close</button>
+              </Link>
+              <div className="search-books-input-wrapper">
+                {/*
+                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  You can find these search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
+                */}
+                <input type="text" placeholder="Search by title or author" onKeyUp={e => this.getResults(e.target.value)} />
+
+              </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid">
+                {this.state.books.map((book)=>(
+                  <Book 
+                    title={book.title} 
+                    id={book.id} 
+                    author={book.authors} 
+                    image={book.imageLinks.thumbnail} 
+                    shelf={book.shelf} 
+                    moveBook={this.moveBook}
+                  />
+                ))}
+              </ol>
+            </div>
+          </div>
+    )
+  }
+}
+                   
+
+export default BookSearch
