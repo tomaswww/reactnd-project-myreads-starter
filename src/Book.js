9712102e680e63fd3e8088ee344f6e118e7be4a2
tomaswww <twingord@gmail.com>
2021-06-12 23:27:35 -0300

    Fixed misplaced div that broke the UI

--- src/Book.js
+++ src/Book.js
@@ -6,8 +6,9 @@ const Book = (props) => {
     return (
             <div className="book">
               <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("'+props.image+'")'}}></div>
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("'+props.image+'")'}}>
                   <ShelfChanger shelf={props.shelf} title={props.title} moveBook={props.moveBook}/>
+                </div>
               </div>
               <div className="book-title">{props.title}</div>
               {props.author.map(author =>
